c851081ae44a30e2585994997a21e7b5b0c82f71
idkV1312 <ivan131206@gmail.com>
2026-04-24 18:01:25 +0400

    Fix main.dart merge conflict and BlocProvider setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:yabudu/features/yabudu/presentation/pages/category_select_screen.dart';
-import 'package:yabudu/features/yabudu/presentation/pages/events_feed_draft_layout.dart';
-import 'package:yabudu/features/yabudu/presentation/pages/glassmorphism_draft_layout.dart';
-import 'package:yabudu/features/yabudu/presentation/pages/poll_draft_layout.dart';
-import 'package:yabudu/features/yabudu/presentation/pages/reg_screen.dart';
-=======
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:yabudu/features/yabudu/presentation/bloc/yabudu_bloc.dart';
-import 'package:yabudu/features/yabudu/presentation/bloc/yabudu_event.dart';
 import 'package:yabudu/features/yabudu/presentation/pages/map_screen.dart';
 import 'package:yabudu/theme/app_theme.dart';
->>>>>>> c97ecd5 (map screen)
 
 void main() {
   runApp(const MainApp());
@@ -22,21 +13,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
-      home: EventsFeedDraftLayout(),
-=======
-      theme: AppTheme.light,
-      home: YandexMapScreen(
-        onShowListTap: () {},
-        filterLabels: const [
-          'Тест-фильтр A',
-          'Фейковая дата',
-          'Demo категория',
-        ],
+    return BlocProvider<YabuduBloc>(
+      create: (_) => YabuduBloc(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light,
+        home: YandexMapScreen(
+          onShowListTap: () {},
+          filterLabels: const [
+            'Тест-фильтр A',
+            'Фейковая дата',
+            'Demo категория',
+          ],
+        ),
       ),
->>>>>>> c97ecd5 (map screen)
     );
   }
 }
